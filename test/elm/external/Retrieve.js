f4144a103e696d77627c605563c49e3cd503a75e
Andrew Bird <andrew.bird@semanticbits.com>
2020-05-12 16:08:59 -0500

    Manually recreated f87e0fb70d3b35ed6a2c5e367b9755c1fe2f1f87
    Convert test data.coffee to data.js

---
+++ test/elm/external/Retrieve.js
@@ -0,0 +1,10 @@
+/*
+   WARNING: This is a GENERATED file.  Do not manually edit!
+
+   To generate this file:
+       - Edit Retrieve.cql to add a CQL Snippet
+       - From generator dir: ./gradlew generateTestData
+*/
+
+/* eslint-disable */
+
