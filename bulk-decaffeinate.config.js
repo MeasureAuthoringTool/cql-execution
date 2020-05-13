903be379bd1f5053db6902ae32bc9f932bb0e52e
Andrew Bird <andrew.bird@semanticbits.com>
2020-05-13 09:24:28 -0500

    Reverting bulk-decaffeinate changes to simplify the conversion: not converting directly to TypeScript, and not using JS Modules. We'll change those manually, file-by file as we clean everything up

--- bulk-decaffeinate.config.js
+++ bulk-decaffeinate.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   mochaEnvFilePattern: '^.*-test.js$',
-  useJSModules: true,
+  useJSModules: false,
   decaffeinateArgs: [
     '--loose-default-params',
     '--loose-for-expressions',
     '--loose-for-of',
     '--loose-includes'
   ],
-  outputFileExtension: 'ts'
+  outputFileExtension: 'js'
 };
